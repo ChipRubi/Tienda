570d54458b79c5065d70b9beff68c95bbf2807c0
Chip <hector21096@gmail.com>
2019-04-16 18:22:59 -0500

    Constantes del modulo generadas. Nombre, Eventos, Vistas

---
+++ modules/inventario/constants.php
@@ -0,0 +1,19 @@
+<?php
+
+// Nombre del modulo
+const MODULO = 'inventario/';
+
+// Controladores de Eventos del Usuario
+const AGREGAR_INVENTARIO = 'set';
+const VER_INVENTARIO = 'get';
+const EDITAR_INVENTARIO = 'edit';
+const BORRAR_INVENTARIO = 'delete';
+
+// Nombre de las vistas
+const VISTA_AGREGAR_INVENTARIO = 'agregar';
+const VISTA_VER_INVENTARIO = 'ver';
+const VISTA_EDITAR_INVENTARIO = 'editar';
+const VISTA_BORRAR_INVENTARIO = 'borrar';
+const VISTA_LISTAR_INVENTARIO = 'listar';
+
+?>
