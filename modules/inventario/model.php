da56d9b13867df299ec8b220fad38749bee67004
Chip <hector21096@gmail.com>
2019-04-21 19:53:59 -0500

    Metodos para OBTENER datos de inventario generados

---
+++ modules/inventario/model.php
@@ -0,0 +1,88 @@
+<?php
+
+require_once('../../core/db_abstract_model.php');
+
+class Inventario extends DBAbstractModel {
+
+    private $id;
+    public $nombre;
+    public $fechaCreacion;
+    public $descripcion;
+    public $idCategoria;
+    public $categoria;
+    public $lista = array();
+
+    public function get($id='') {
+        if ($id != '') {
+            $this->query = "
+                SELECT
+                    id_inv as id,
+                    nombre_inv as nombre,
+                    creacion_inv as fechaCreacion,
+                    descripcion_inv as descripcion,
+                    id_cat_inv as idCategoria,
+                    nombre_cat as categoria
+                FROM tienda.tda_tbl_inventario
+                INNER JOIN tienda.tda_tbl_categoria ON id_cat_inv = id_cat
+                WHERE id_inv = '$id'
+            ";
+            $this->getResultsFromQuery();
+        }
+
+        if (count($this->rows) == 1) {
+            foreach ($this->rows[0] as $propiedad => $valor) {
+                if ($propiedad == 'id') {
+                    $this->id = $valor;
+                } else if ($propiedad == 'nombre') {
+                    $this->nombre = $valor;
+                } else if ($propiedad == 'fechaCreacion') {
+                    $this->fechaCreacion = $valor;
+                } elseif ($propiedad == 'descripcion') {
+                    $this->descripcion = $valor;
+                } elseif ($propiedad == 'idCategoria') {
+                    $this->idCategoria = $valor;
+                } elseif ($propiedad == 'categoria') {
+                    $this->categoria = $valor;
+                }
+            }
+        }
+    }
+
+    public function set() {
+
+    }
+
+    public function edit() {
+
+    }
+
+    public function delete($id='') {
+
+    }
+
+    public function getAll() {
+        $this->query = "
+            SELECT *
+            FROM tienda.tda_tbl_inventario
+            INNER JOIN tienda.tda_tbl_categoria ON id_cat_inv = id_cat
+        ";
+        $this->getResultsFromQuery();
+        if (count($this->rows) >= 1) {
+            $this->lista = $this->rows;
+        } else {
+            $this->mensaje = 'No hay ningun inventario';
+        }
+    }
+
+    public function existeInventario($datosInventario=array()){
+
+    }
+
+    function __construct() {
+        $this->db_name = 'tienda';
+    }
+
+    function __destruct() {}
+}
+
+?>
